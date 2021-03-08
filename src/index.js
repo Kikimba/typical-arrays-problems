7db93aed591feccb0e598d01a3fb0da6bcfd8e36
Kikimba <kobra.ven213@gmail.com>
2021-03-08 17:41:21 +0600

    fix: fiexd minimum array

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 exports.min = function min (array) {
   if (!Array.isArray(array)){
-  return 0
+    return 0
   }
   else{
   let result = array[0];
@@ -15,17 +15,17 @@ exports.min = function min (array) {
 
 // Maximum
 exports.max = function max (array) {
-  if (Array.isArray(array)){
+  if (!Array.isArray(array)){
     return 0
   }
   else{
-    let res = array[0];
+    let result = array[0];
     for(let i= 0; i < array.length; i++){
-      if ((array[i]) > res){
-        res = array[i];
+      if ((array[i]) > result){
+        result = array[i];
       }   
     }
-return res;
+return result;
   }   
 }
 
@@ -35,10 +35,10 @@ exports.avg = function avg (array) {
     return 0
   }
   else{
-    let res = 0;
+    let result = 0;
     for(let i= 0; i < array.length; i++){
-      res += array[i]; 
+      result += array[i]; 
     }
-  return res/array.length;
+  return result/array.length;
   }
 }
