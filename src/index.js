dc1f7bf0abe0fc9389c91328bfb42cdeced8bc81
Kikimba <kobra.ven213@gmail.com>
2021-03-08 17:37:28 +0600

    feat: Finished 1 and 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)){
+  return 0
+  }
+  else{
+  let result = array[0];
+  for(let i=0; i<array.length; i++){
+    if ((array[i]) < result){
+      result = array[i];
+    }
+  }
+  return result;
+  }
 }
 
+// Maximum
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array)){
+    return 0
+  }
+  else{
+    let res = array[0];
+    for(let i= 0; i < array.length; i++){
+      if ((array[i]) > res){
+        res = array[i];
+      }   
+    }
+return res;
+  }   
 }
 
+// Average
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)){
+    return 0
+  }
+  else{
+    let res = 0;
+    for(let i= 0; i < array.length; i++){
+      res += array[i]; 
+    }
+  return res/array.length;
+  }
 }
